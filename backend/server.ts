519b4fd5b39725fd28a220e86aa38d465dce2080
Kevin Liu <kevinliu676@yahoo.ca>
2024-05-22 17:54:35 -0400

    boilerplate backend

---
+++ backend/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true}));
+app.use(cors());
+app.use(express.json());
