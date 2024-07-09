49b50829fad16cfc1c7d9c8f8200546366c5483c
samsonshaji <samsonrobert6868@gmail.com>
2024-07-08 23:32:30 -0400

    added text fields and buttons for features 7, 9

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -25,6 +25,17 @@ export default function Home() {
   const [relatedTags, setRelatedTags] = useState<any>(null);
   const [relatedTagsTag, setRelatedTagsTag] = useState("");
 
+  const [mentionedTags, setMentionedTags] = useState<any>(null);
+  const [mentionedTagsTag, setMentionedTagsTag] = useState("");
+
+  // const [authorsAnswers, setAuthorsAnswers] = useState<any>(null);
+  // const [answerID, setAnswerID] = useState("");
+  // const [answerTag, setAnswerTag] = useState("");
+
+
+  const [redemptionComment, setRedemptionComment] = useState<any>(null);
+  const [redemptionUsername, setRedemptionUsername] = useState("");
+
   const handleActivitySubmit = async (event: FormEvent<HTMLFormElement>) => {
     event.preventDefault();
 
@@ -152,7 +163,49 @@ export default function Home() {
       console.error("Error:", error);
     }
   };
+  
+  const handleMentionedTagsSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/getmentionedtags/${mentionedTagsTag}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      setMentionedTags(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
 
+  const handleRedemptionCommentSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+  
+    try {
+      const response = await fetch(
+        `http://localhost:3000/api/getredemptioncomment/${redemptionUsername}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const data = await response.json();
+      console.log(data);
+      setRedemptionComment(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+    
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>
@@ -405,7 +458,7 @@ export default function Home() {
           >
             Get Related Tags
           </button>
-        </form>
+        </form> 
 
         {relatedTags && (
           <div className="mt-4">
@@ -422,6 +475,84 @@ export default function Home() {
                   <tr key={index}>
                     <td className="py-2 px-4 border-b">{tag.tagName}</td>
                     <td className="py-2 px-4 border-b">{tag.freq}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+        
+        <form onSubmit={handleMentionedTagsSubmit} className="mt-8">
+          <input
+            className="py-2 px-4 mb-4 rounded border border-gray-400 mr-2"
+            type="text"
+            placeholder="Enter tag"
+            value={mentionedTagsTag}
+            onChange={(e) => setMentionedTagsTag(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-orange-500 hover:bg-orange-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Get Mentioned Tags
+          </button>
+        </form>
+
+        {mentionedTags && (
+          <div className="mt-4">
+            <h2>Mentioned Tags:</h2>
+            <table className="min-w-full bg-white">
+              <thead>
+                <tr>
+                  <th className="py-2 px-4 border-b">Tag Name</th>
+                  <th className="py-2 px-4 border-b">Frequency</th>
+                </tr>
+              </thead>
+              <tbody>
+                {mentionedTags.tags.map((tag: any, index: number) => (
+                  <tr key={index}>
+                    <td className="py-2 px-4 border-b">{tag.tagName}</td>
+                    <td className="py-2 px-4 border-b">{tag.freq}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        )}
+
+        <form onSubmit={handleRedemptionCommentSubmit} className="mt-8">
+          <input
+            className="py-2 px-4 mb-4 rounded border border-gray-400 mr-2"
+            type="text"
+            placeholder="Enter username"
+            value={redemptionUsername}
+            onChange={(e) => setRedemptionUsername(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-blue-800 hover:bg-blue-500 text-white font-bold py-2 px-4 rounded"
+          >
+            Get Redemption Comment
+          </button>
+        </form>
+
+        {redemptionComment && (
+          <div className="mt-4">
+            <h2>Redemption Comment:</h2>
+            <table className="min-w-full bg-white">
+              <thead>
+                <tr>
+                  <th className="py-2 px-4 border-b">Comment ID</th>
+                  <th className="py-2 px-4 border-b">Relative Diff</th>
+                  <th className="py-2 px-4 border-b">Body</th>
+                </tr>
+              </thead>
+              <tbody>
+                {redemptionComment.comment.map((comment: any, index: number) => (
+                  <tr key={index}>
+                    <td className="py-2 px-4 border-b">{comment.commentID}</td>
+                    <td className="py-2 px-4 border-b">{comment.rel_diff}</td>
+                    <td className="py-2 px-4 border-b">{comment.body}</td>
                   </tr>
                 ))}
               </tbody>
