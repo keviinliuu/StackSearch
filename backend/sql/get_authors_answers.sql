f1864479d8455f203f26b712fbd50cefe1f4bf29
DongtaiS <dongtaishi.w@gmail.com>
2024-07-23 23:39:43 -0400

    get_authors_answers optimization

--- backend/sql/get_authors_answers.sql
+++ backend/sql/get_authors_answers.sql
@@ -1,13 +1,30 @@
 WITH authID AS (SELECT authorID FROM stackoverflow_production.Answers WHERE postID = @postID),
-tagID AS (SELECT tagID FROM stackoverflow_production.Tags WHERE tagName = @tag)
+tagID AS (SELECT tagID FROM stackoverflow_production.Tags WHERE tagName = @tag),
+relevantAnswers AS (
 SELECT
-a.postID,
-a.body,
-a.authorID
+  a.postID,
+  a.body,
+  a.authorID,
+  a.questionID
 FROM
-stackoverflow_production.Answers a
-JOIN (SELECT postID FROM stackoverflow_production.Questions) q ON a.questionID = q.postID
+ stackoverflow_production.Answers a
 WHERE
-a.authorID IN (SELECT * FROM authID)
-AND (SELECT * FROM tagID) IN (SELECT qt.tagID FROM stackoverflow_production.QuestionTags qt WHERE qt.postID = q.postID)
+  a.authorID = (SELECT * FROM authID) 
+),
+relevantQuestions AS (
+SELECT
+  q.postID
+FROM
+  stackoverflow_production.Questions q
+  JOIN stackoverflow_production.QuestionTags qt ON q.postID = qt.postID
+WHERE
+  qt.tagID = (SELECT * FROM tagID) 
+)
+SELECT
+  a.postID,
+  a.body,
+  a.authorID
+FROM
+  relevantAnswers a
+  JOIN relevantQuestions q ON a.questionID = q.postID
 LIMIT 10;
