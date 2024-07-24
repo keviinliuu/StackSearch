f5f89c391b6b00245bf8b4524123eed50785980d
Alysha <113640728+alyshawang@users.noreply.github.com>
2024-07-23 20:46:34 -0400

    Fixed navbar spacing and added post summary to homepage

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,10 +1,12 @@
 "use client";
+import Summary from "./components/Summary";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <div className="gradient"></div>
       <div className="max-w-6xl w-full mx-auto">
+        <Summary/>
       </div>
     </main>
   );
